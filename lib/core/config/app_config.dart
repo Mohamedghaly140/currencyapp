348a6322ab727c40528fb674614e8c73d5622a85
mohamedghaly140 <mohamedghaly140@gmail.com>
2025-09-12 16:52:07 +0300

    Add apiKey to AppConfig and update DevelopmentAppConfig setup
    - Introduced apiKey in AppConfig for environment variable management.
    - Updated DevelopmentAppConfig to set apiKey from EnvDev configuration.
    - Ensured proper integration of new environment variables for development setup.

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,3 +1,4 @@
 abstract class AppConfig {
   static String apiUrl = '';
+  static String apiKey = '';
 }
--- lib/core/config/config_dev.dart
+++ lib/core/config/config_dev.dart
@@ -1,7 +1,9 @@
 import 'package:currencyapp/core/config/app_config.dart';
+import 'package:currencyapp/core/config/env_dev.dart';
 
 class DevelopmentAppConfig {
   static Future<void> setup() async {
     AppConfig.apiUrl = 'https://prepaid.currconv.com';
+    AppConfig.apiKey = EnvDev.apiKey;
   }
 }
