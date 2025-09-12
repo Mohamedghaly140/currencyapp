8f015ea324d6916281bd6bab0ccd704214a7de93
mohamedghaly140 <mohamedghaly140@gmail.com>
2025-09-12 18:12:36 +0300

    Add countries endpoint to EndPoints class
    - Introduced a new static constant for the countries API endpoint in the EndPoints class to support fetching country data.

--- lib/core/networking/end_points.dart
+++ lib/core/networking/end_points.dart
@@ -1,5 +1,6 @@
 class EndPoints {
   static const String usage = '/others/usage';
   static const String convert = '/api/v8/convert';
+  static const String countries = '/api/v8/countries';
   static const String currencies = '/api/v8/currencies';
 }
